ec0ec9518f59e03c695c9a44d66649fca7a57f8a
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-05-19 13:49:14 +0300

    Completed assignment

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,21 @@
 from user import User
 
 class Student(User):
+    knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
+    def __init__(self,first_name,last_name):
+        super().__init__(first_name,last_name)
+        self.knowledge = Student.knowledge
+
     
-    def learn(self):
+    def learn(self,str):
+        return self.knowledge.append(str)
         pass
